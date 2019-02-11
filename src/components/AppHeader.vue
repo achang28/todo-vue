3af519aee639727d7be8e5eef3befb453547edbe
Albert Chang <albert_w_chang@apple.com>
2019-02-10 20:50:34 -0600

    Partially through setting up menu filter

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="ui secondary pointing menu">
-    <h2 class="ui header">Todo List</h2>
-    <span class="right menu">
+    <a :index='index'
+       :option='option'
+       @click='setActive'
+       v-for='(option, index) in menuOptions'
+       :class="index === activeIndex ? 'active item' : 'item'">{{option}}</a>
+    <span class='right menu'>
       <a v-if="isLoggedIn" href="#" @click="logout">Logout</a>
       <a v-else href="#" class="ui item" @click="login">Login</a>
     </span>
@@ -10,10 +14,22 @@
 
 <script>
   import { mapActions, mapGetters } from 'vuex';
+
   export default {
+    data: () => ({
+      menuOptions: ['All', 'Active', 'Done'],
+      activeIndex: 0
+    }),
     name: "Header",
-    computed: mapGetters(['isLoggedIn']),
-    methods: mapActions(['login', 'logout']),
+    computed: {
+      ...mapGetters(['isLoggedIn'])
+    },
+    methods: {
+      ...mapActions(['login', 'logout']),
+      setActive() {
+        debugger;
+      }
+    },
     beforeUpdate() {
       window.console.log("*** Header is being updated ***");
     }
